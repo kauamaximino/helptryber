2eb6fe6d73f8cf162e05e3008faaac76b9398f2e
Kaua Maximino <kaua_maximino@outlook.com>
2022-02-11 16:55:56 -0300

    criando JS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const projetosBloco1 = require('/home/kaua/Área de Trabalho/Projetos Pessoais/projeto-bonus-semama-ontrack/helptryber/projetosBloco1.js')
 
 function createproject {
-  
+  const conteudo = 
 }
